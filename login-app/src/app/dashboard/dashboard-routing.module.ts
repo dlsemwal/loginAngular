33efd1dac1bc7aa2250eef1a80558c88c948c164
Divyanshu Semwal <dlsemwal@gmail.com>
2019-07-04 07:39:46 +0530

    bootstrap has been installed

--- login-app/src/app/dashboard/dashboard-routing.module.ts
+++ login-app/src/app/dashboard/dashboard-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from '../guards/auth-guard.service';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
-    path: '', component: HomeComponent,
+    path: '', component: LayoutComponent,
   //  canActivate: [AuthGuard]
   }
 ];
